a6927cadb8680adaa1203cfbd8ce974d2f7fa288
Daniel Ferraz <daniel@dcoferraz.com>
2017-11-29 15:41:00 -0200

    MEAN Server - Google Auth

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
+// Load Models
+require('./models/User');
 
 // Passport config
 require('./config/passport')(passport);
@@ -9,10 +13,38 @@ require('./config/passport')(passport);
 // Load Routes
 const auth = require('./routes/auth');
 
+// Load Keys
+const keys = require('./config/keys');
+
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+  useMongoClient:true
+})
+  .then(() => console.log('|   DB-INFO   | MongoDB Connected'))
+  .catch(err => console.log(err));
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('It works');
+  res.send('|     INFO    | It works');
+});
+
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
 });
 
 // Use Routes
@@ -22,5 +54,5 @@ const port = process.env.PORT || 5000;
 
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`| SERVER-INFO | Server started on port ${port}`);
 });
